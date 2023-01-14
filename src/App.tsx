dded300ae46d0474f7a1255ca894a230a08de868
MatthewKK01 <mateyavelashvili@gmail.com>
2023-01-14 12:23:37 +0400

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import sunIcon from "./images/icon-sun.svg";
 import moonIcon from "./images/icon-moon.svg";
 import ITask from "./interfaces";
 import ToDoTask from "./Components/ToDoTask";
+import check from "./images/icon-check.svg";
 
 function App() {
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState<boolean>(false);
   const [task, setTask] = useState("");
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [checked, setChecked] = useState(false);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value);
@@ -24,18 +26,24 @@ function App() {
       },
       taskName: task,
     };
-    setTodoList([...todoList, newTask]);
-    setTask("");
+    if (task !== "") {
+      setTodoList([...todoList, newTask]);
+      setTask("");
+    } else {
+      return null;
+    }
   };
 
-  const deleteTask = (taskNametoDelete:string):void => {
-    setTodoList(todoList.filter((task)=>{
-      return task.taskName !== taskNametoDelete;
-    }))
-}
+  const deleteTask = (taskNametoDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNametoDelete;
+      })
+    );
+  };
 
   return (
-    <>
+    <div className={`App ${dark ? `bg-[#171823]` : `bg-white`}`}>
       <header className="relative">
         <img
           className="absolute min-[375px]:w-full md:hidden"
@@ -47,7 +55,8 @@ function App() {
           src={dark ? DarkDesktop : lightDesktop}
           alt=""
         />
-        <div className="max-width relative flex justify-between items-center top-12 xl:top-[70px]">
+
+        <div className="max-width relative flex justify-between items-center top-12 xl:top-[70px] ">
           <p className="text-white uppercase text-3xl font-medium">Todo</p>
           <img
             className="cursor-pointer h-6"
@@ -56,18 +65,59 @@ function App() {
             alt=""
           />
         </div>
-        <div className="input-container relative">
-          <input type="text" name="todo" value={task} onChange={handleChange} />
-          <button onClick={addTask}>add task</button>
+        <div className=" container p-6 relative top-16">
+          <div
+            className={`input-container relative rounded py-6 pl-6 flex items-center ${
+              dark ? `bg-[#25273D]` : `bg-white`
+            }`}
+          >
+            <span
+              className={`w-5 h-5 inline-block rounded-full border-2 mr-3 cursor-pointer relative ${dark && `border-[#393A4B]`} ${checked && `bg-red-500`}`}
+              onClick={() => {
+                setChecked(!checked);
+              }}
+            >
+              <img src={check} className={`check-image ${checked ? `block` : `hidden`}`} alt="check" />
+            </span>
+            <input
+              type="text"
+              className={`text-xs ${dark && `bg-[#25273D]`}`}
+              name="todo"
+              placeholder="Create a new todo…"
+              value={task}
+              onChange={handleChange}
+            />
+            <button onClick={addTask}>add task</button>
+          </div>
         </div>
       </header>
 
-      <div className="tasklist">
+      <div className="tasklist container relative top-8 p-6">
         {todoList.map((task: ITask, key: number) => {
-          return <ToDoTask deleteTask={deleteTask} key={key} task={task} />;
+          return (
+            <ToDoTask
+              dark={dark}
+              deleteTask={deleteTask}
+              setDark={setDark}
+              key={key}
+              task={task}
+              checked={checked}
+              setChecked={setChecked}
+            />
+          );
         })}
+        <div
+          className={`flex justify-between px-5 py-4 items-center border-b-2 rounded-t-none rounded-md ${
+            dark ? `bg-[#25273D] border-none` : `bg-white`
+          }`}
+        >
+          <p className="text-[#9495A5] text-xs">{todoList.length} items left</p>
+          <p className="text-[#9495A5] text-xs cursor-pointer">
+            Clear Completed
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Components/ToDoTask.tsx
+++ src/Components/ToDoTask.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import ITask from '../interfaces';
+import React, { useState } from "react";
+import ITask from "../interfaces";
+import check from "../images/icon-check.svg";
 
-interface Props{
-    task:ITask;
-    deleteTask(taskNametoDelete:string): void;
+interface Props {
+  task: ITask;
+  deleteTask(taskNametoDelete: string): void;
+  dark: any;
+  setDark : any;
+  checked: boolean;
+  setChecked: any;
 }
 
-function ToDoTask({task,deleteTask}:Props) {
+function ToDoTask({ task, deleteTask, dark, setDark,checked, setChecked}: Props) {
+
+  const [isChecked , setIsChecked] = useState(false);
+  const [isCompleted,setIsCompleted] = useState(false);
+
+  const completed = () => {
+    setChecked(!isChecked);
+    setIsCompleted(!isCompleted)
+  }
+
   return (
-    <ul className='flex justify-between items-center bg-white p-4'>
-      <li>{task.taskName}</li>
-      <button onClick={()=> deleteTask(task.taskName)}>&#x2715;</button>
-    </ul>
-  )
+    <>
+      <ul
+        className={`flex justify-between items-center relative ${
+          dark ? `bg-[#25273D] text-[#C8CBE7] border-b-[#393A4B]` : `bg-white`
+        } border-b-2 rounded-b-none rounded-md p-4`}
+      >
+        <span onClick={completed} className="w-5 top-[17.5px] bg-black h-5  absolute rounded-full border-2 mr-3 cursor-pointer">
+          <img src={check} alt="check" className="check" />
+        </span>
+        <li className={`pl-8 ${dark ? `text-[#C8CBE7]` : `text-[#494C6B]`} ${isCompleted && `line-through`}`}>{task.taskName}</li>
+        <button onClick={() => deleteTask(task.taskName)}>&#x2715;</button>
+      </ul>
+    </>
+  );
 }
 
-export default ToDoTask
+export default ToDoTask;
